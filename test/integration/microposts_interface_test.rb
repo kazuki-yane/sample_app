6ae502857f57edc1371adfccbcfe34f87bc1cdf1
kazuki-yane <yanekazuki@yahoo.co.jp>
2018-08-29 14:14:38 +0900

    テストの編集

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -20,7 +20,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     content = "This micropost really ties the room together"
     picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content, picture: picture }
+      post microposts_path, params:{micropost: { content: content, picture: picture }}
     end
     assert assigns(:micropost).picture?
     assert_redirected_to root_url
